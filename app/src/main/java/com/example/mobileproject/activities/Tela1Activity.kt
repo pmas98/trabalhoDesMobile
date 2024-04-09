438d3d093d73ce462667c480c8d5224656af7bb0
Evandro Araújo Luz <>
2024-04-09 08:22:58 -0300

    second commit

---
+++ app/src/main/java/com/example/mobileproject/activities/Tela1Activity.kt
@@ -0,0 +1,12 @@
+package com.example.mobileproject.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobileproject.R
+
+class Tela1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela1)
+    }
+}
