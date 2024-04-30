1487a9fe22477b3e06971ad7603f21c81adfda86
filipe-ts <filipets@live.com>
2024-04-30 20:13:48 -0300

    parte logica da tela 5

---
+++ app/src/main/java/com/example/mobileproject/activities/SelecaoObras2.kt
@@ -0,0 +1,12 @@
+package com.example.mobileproject.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobileproject.R
+
+class SelecaoObras2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selecao_obras2)
+    }
+}
