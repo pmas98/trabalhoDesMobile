84574edd7741561ae09776ff8646983e738681ae
Evandro Araújo Luz <>
2024-05-16 09:13:22 -0300

    download direto do qrcode

--- app/src/main/java/com/example/mobileproject/activities/AtalhosAPI.kt
+++ app/src/main/java/com/example/mobileproject/activities/AtalhosAPI.kt
@@ -1,5 +1,16 @@
 package com.example.mobileproject.activities
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import com.example.mobileproject.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.Call
@@ -11,6 +22,9 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import okio.IOException
+import java.io.File
+import java.io.FileOutputStream
+
 class AtalhosAPI() {
     fun verExpos() {
         val url = "https://backendapp-production-da1c.up.railway.app/expo"
@@ -22,14 +36,14 @@ class AtalhosAPI() {
             .build()
         Log.d("OkHTTP", request.toString())
 
-        client.newCall(request).enqueue(object: Callback {
+        client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: java.io.IOException) {
                 e.printStackTrace()
                 Log.d("OkHTTP", "fail2")
             }
 
             override fun onResponse(call: Call, response: Response) {
-                if (!response.isSuccessful){
+                if (!response.isSuccessful) {
                     throw IOException("Unexpected code $response")
                 }
 
@@ -40,6 +54,7 @@ class AtalhosAPI() {
             }
         })
     }
+
     fun apagarExpo(id: String) {
         val url = "https://backendapp-production-da1c.up.railway.app/expo"
         Log.d("OkHTTP", url)
@@ -58,14 +73,14 @@ class AtalhosAPI() {
             .build()
         Log.d("OkHTTP", request.toString())
 
-        client.newCall(request).enqueue(object: Callback {
+        client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: java.io.IOException) {
                 e.printStackTrace()
                 Log.d("OkHTTP", "fail2")
             }
 
             override fun onResponse(call: Call, response: Response) {
-                if (!response.isSuccessful){
+                if (!response.isSuccessful) {
                     throw IOException("Unexpected code $response")
                 }
 
@@ -77,7 +92,13 @@ class AtalhosAPI() {
         })
     }
 
-    suspend fun addObra(id: String, name: String, autor: String, description: String, imageURL: String ) {
+    suspend fun addObra(
+        id: String,
+        name: String,
+        autor: String,
+        description: String,
+        imageURL: String
+    ) {
         val url = "https://backendapp-production-da1c.up.railway.app/obra"
         Log.d("OkHTTP", url)
         val client = OkHttpClient()
@@ -98,14 +119,14 @@ class AtalhosAPI() {
             .build()
         Log.d("OkHTTP", request.toString())
 
-        client.newCall(request).enqueue(object: Callback {
+        client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: java.io.IOException) {
                 e.printStackTrace()
                 Log.d("OkHTTP", "fail2")
             }
 
             override fun onResponse(call: Call, response: Response) {
-                if (!response.isSuccessful){
+                if (!response.isSuccessful) {
                     throw IOException("Unexpected code $response")
                 }
 
@@ -117,7 +138,7 @@ class AtalhosAPI() {
         })
     }
 
-    suspend fun addObra(obraData: MutableMap<String,String>): String {
+    suspend fun addObra(obraData: MutableMap<String, String>): String {
 
         val url = "https://backendapp-production-da1c.up.railway.app/obra"
         Log.d("OkHTTP", url)
@@ -146,7 +167,7 @@ class AtalhosAPI() {
             client.newCall(request).execute()
         }
 
-        if (!response.isSuccessful){
+        if (!response.isSuccessful) {
             Log.d("MYmobileproject", "Not successul ${response}")
 //        throw IOException("Unexpected code $response")
         }
@@ -155,7 +176,7 @@ class AtalhosAPI() {
 
     }
 
-    suspend fun deleteObra(obraId: String): String{
+    suspend fun deleteObra(obraId: String): String {
 
         var respostaFinal = "Sem Resposta"
 
@@ -179,7 +200,7 @@ class AtalhosAPI() {
             client.newCall(request).execute()
         }
 
-        if (!response.isSuccessful){
+        if (!response.isSuccessful) {
             return "Unexpected code ${response}"
 //        throw IOException("Unexpected code $response")
         }
@@ -190,6 +211,56 @@ class AtalhosAPI() {
 
         return respostaFinal
 
+    }
+
+    fun downloadQR(obraId: String, context: Context) {
+
+        val client = OkHttpClient()
+
+        val url = "https://backendapp-production-da1c.up.railway.app/qrcode?id=${obraId}"
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: java.io.IOException) {
+                // Handle failure
+                e.printStackTrace()
+                Log.d("OkHTTP", "sem resposta")
+            }
+
+            @RequiresApi(Build.VERSION_CODES.Q)
+            override fun onResponse(call: Call, response: Response) {
+                if (!response.isSuccessful) {
+                    Log.d("OkHTTP", "QR: erro no ciente")
+                    return
+                }
+
+                val resolver = context.contentResolver
+                val contentValues = ContentValues().apply {
+                    put(
+                        MediaStore.MediaColumns.DISPLAY_NAME,
+                        "qr_${obraId}.png"
+                    ) // The name of the file
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/png") // The MIME type of the file
+                    put(
+                        MediaStore.MediaColumns.RELATIVE_PATH,
+                        Environment.DIRECTORY_DOWNLOADS
+                    ) // The relative path to the file within the external storage
+                }
+
+                val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+
+                val responseBody = response.body?.bytes() // Get the bytes of the response body
+
+                if (responseBody != null) {
+                    resolver.openOutputStream(uri!!)?.use { outputStream ->
+                        outputStream.write(responseBody)
+                    }
+                }
+            }
+        })
     }
 
 }
