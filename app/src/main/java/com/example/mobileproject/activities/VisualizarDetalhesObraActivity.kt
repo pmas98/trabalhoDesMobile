a3cc164f25e1208e5597166b541ed360d5eda2af
Evandro Araújo Luz <>
2024-05-21 08:51:46 -0300

    layout da tela de informações da obra completo

--- app/src/main/java/com/example/mobileproject/activities/VisualizarDetalhesObraActivity.kt
+++ app/src/main/java/com/example/mobileproject/activities/VisualizarDetalhesObraActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mobileproject.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobileproject.R
 
@@ -8,6 +10,17 @@ class VisualizarDetalhesObraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visualizar_detalhes_obra)
+
+        findViewById<ImageButton>(R.id.inicioButton)
+            .setOnClickListener {
+                startActivity(Intent(this, UserMenuActivity::class.java))
+            }
+
+        findViewById<ImageButton>(R.id.button_play_audio)
+            .setOnClickListener {
+                // tocar audio
+            }
+
 
     }
 }
